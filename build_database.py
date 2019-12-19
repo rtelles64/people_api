67c15ab340cfcfe593e983d1939d68bcf3edfde0
Roy Telles <x-rtelles@4353-laptop.local>
2019-12-19 08:01:09 -0800

    Update the People data structure to set up relationships between person and note tables

--- build_database.py
+++ build_database.py
@@ -7,9 +7,37 @@ from models import Person
 
 # Data to initialize database with
 PEOPLE = [
-    {'fname': 'Doug', 'lname': 'Farrell'},
-    {'fname': 'Kent', 'lname': 'Brockman'},
-    {'fname': 'Bunny', 'lname': 'Easter'}
+    {
+        "fname": "Doug",
+        "lname": "Farrell",
+        "notes": [
+            ("Cool, a mini-blogging application!", "2019-01-06 22:17:54"),
+            ("This could be useful", "2019-01-08 22:17:54"),
+            ("Well, sort of useful", "2019-03-06 22:17:54"),
+        ],
+    },
+    {
+        "fname": "Kent",
+        "lname": "Brockman",
+        "notes": [
+            (
+                "I'm going to make really profound observations",
+                "2019-01-07 22:17:54",
+            ),
+            (
+                "Maybe they'll be more obvious than I thought",
+                "2019-02-06 22:17:54",
+            ),
+        ],
+    },
+    {
+        "fname": "Bunny",
+        "lname": "Easter",
+        "notes": [
+            ("Has anyone seen my Easter eggs?", "2019-01-07 22:47:54"),
+            ("I'm really late delivering these!", "2019-04-06 22:17:54"),
+        ],
+    },
 ]
 
 # Delete database file if it exists currently
